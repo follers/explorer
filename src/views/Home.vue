44518ad38b498c7bda23e0ed704908ee93b0a9e0
foller <sergeyfoller@gmail.com>
2022-10-09 17:42:57 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          Fexp Dashboard<small class="flow-left">Beta</small>
+           Fexp Dashboard<small class="flow-left">Beta</small>
         </h1>
       </div>
     </b-link>
